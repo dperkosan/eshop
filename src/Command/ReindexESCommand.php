d90d5ec4474e46bc3bd703908aed83fe47777ecf
Damir Perkosan <damir.perkosan@diwanee.com>
2020-02-04 14:12:12 +0100

    map

--- src/Command/ReindexESCommand.php
+++ src/Command/ReindexESCommand.php
@@ -170,6 +170,14 @@ class ReindexESCommand extends Command
         // required_options ?
         // msrp_display_actual_price_type ?
         // tax_class_id ?
+        // material
+        // eco_collection
+        // performance_fabric
+        // new
+        // sale
+        // style_general
+        // pattern
+        // climate
         foreach($products as $product){
             $type_id = $product['configurable'] > 0 ? "configurable" : "simple";
             $has_options = $product['configurable'] > 0 ? "1" : "0";
@@ -227,19 +235,44 @@ class ReindexESCommand extends Command
                 "pattern": null,
                 "climate": [],
                 "slug" : "'.$product['url_key'].'",
-
+                "links": {},
+                "stock": [
+                  {
+                    "item_id": '.$product['id'].',
+                    "product_id": '.$product['id'].',
+                    "stock_id": 1,
+                    "qty": 0,
+                    "is_in_stock": true,
+                    "is_qty_decimal": false,
+                    "show_default_notification_message": false,
+                    "use_config_min_qty": true,
+                    "min_qty": 0,
+                    "use_config_min_sale_qty": 1,
+                    "min_sale_qty": 1,
+                    "use_config_max_sale_qty": true,
+                    "max_sale_qty": 10000,
+                    "use_config_backorders": true,
+                    "backorders": 0,
+                    "use_config_notify_stock_qty": true,
+                    "notify_stock_qty": 1,
+                    "use_config_qty_increments": true,
+                    "qty_increments": 0,
+                    "use_config_enable_qty_inc": true,
+                    "enable_qty_increments": false,
+                    "use_config_manage_stock": true,
+                    "manage_stock": true,
+                    "low_stock_date": null,
+                    "is_decimal_divided": false,
+                    "stock_status_changed_auto": 0
+                  }
+                ]
                 "url_path" : "products/'.$product['url_key'].'",
                 "price_incl_tax": null,
                 "special_price_incl_tax": null,
                 "special_to_date": null,
                 "special_from_date": null,
                 "category" : ['.implode(",",$categoriesArr).'],
-                "stock": [
-                  {
-                    "is_in_stock": true,
-                    "qty": 10
-                  }
-                ]
+                
             }';
             
             $result = $this->qryES('POST', 'vue_storefront_catalog_product/_doc/'.$product['id'], $qry);

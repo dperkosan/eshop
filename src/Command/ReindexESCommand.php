74ef53ba0073e65642bd2f87efc21177f6dffc28
Damir Perkosan <dperkosan@gmail.com>
2020-02-05 15:54:23 +0100

    map

--- src/Command/ReindexESCommand.php
+++ src/Command/ReindexESCommand.php
@@ -155,6 +155,59 @@ class ReindexESCommand extends Command
         $output->writeln([
             '============',
             '',
+            'Insert attributes into ES',
+            '============',
+            '',
+        ]);
+
+        $attributes = $this->getAttributes();
+        foreach($attributes as $attribute){
+            $options = $this->getOptions($attribute['id']);
+            $qry = '
+            {
+                "is_wysiwyg_enabled": false,
+                "is_html_allowed_on_front": true,
+                "used_for_sort_by": false,
+                "is_filterable": true,
+                "is_filterable_in_search": false,
+                "is_used_in_grid": true,
+                "is_visible_in_grid": false,
+                "is_filterable_in_grid": true,
+                "position": 0,
+                "apply_to": [
+                    "simple","virtual","bundle","downloadable","configurable"
+                ],
+                "is_searchable": "0",
+                "is_visible_in_advanced_search": "0",
+                "is_comparable": "0",
+                "is_used_for_promo_rules": "1",
+                "is_visible_on_front": "0",
+                "used_in_product_listing": "1",
+                "is_visible": true,
+                "scope": "global",
+                "attribute_id": '.$attribute['id'].',
+                "attribute_code": "'.$attribute['code'].'",
+                "frontend_input": "select",
+                "entity_type_id": null,
+                "is_required": false,
+                "options": ['.$options.'],
+                "is_user_defined": true,
+                "default_frontend_label": "'.$attribute['name'].'",
+                "frontend_labels": null,
+                "backend_type": "int",
+                "source_model": null,
+                "default_value": "'.$attribute['value'].'",
+                "is_unique": "0",
+                "validation_rules": [],
+                "id": '.$attribute['id'].'
+            }';
+            
+            $result = $this->qryES('POST', 'vue_storefront_catalog_category/_doc/'.$attribute['id'], $qry);
+        }
+
+        $output->writeln([
+            '============',
+            '',
             'Insert products into ES',
             '============',
             '',
@@ -270,7 +323,7 @@ class ReindexESCommand extends Command
                   }
                 ],
                 "media_gallery" : ['.$media.'],
-                "configurable_children" : [],
+                "configurable_children" : ['.$configurableChildren.'],
                 "url_path" : "products/'.$product['url_key'].'",
                 "price_incl_tax": null,
                 "special_price_incl_tax": null,
@@ -384,6 +437,44 @@ class ReindexESCommand extends Command
         }
 
         return implode(",", $media);
+    }
+
+    private function getAttributes(){
+        $conn = $this->em->getConnection();
+
+        $sql = 'SELECT o.id, o.code, t.name, v.id AS value
+                FROM sylius_product_option o 
+                LEFT JOIN sylius_product_option_translation t ON t.translatable_id = o.id AND t.locale = "en_US" 
+                LEFT JOIN sylius_product_option_value v ON v.option_id = o.id GROUP BY o.id';
+        
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+        $attributes = $stmt->fetchAll();
+
+        return $attributes;
+    }
+
+    private function getOptions($attributeId){
+        $options = [];
+        $conn = $this->em->getConnection();
+
+        $sql = 'SELECT v.id, t.value 
+        FROM sylius_product_option_value v
+        LEFT JOIN sylius_product_option_value_translation t ON t.translatable_id = v.id AND t.locale = "en_US"
+        WHERE option_id = '.$attributeId;
+        
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+        $results = $stmt->fetchAll();
+
+        foreach($results as $key => $result){
+            $options[] = '{
+                "label": "'.$result['value'].'",
+                "value": "'.$result['id'].'"
+              }';
+        }
+
+        return implode(",", $options);
     }
 
     private function getUrlKey($slug){
